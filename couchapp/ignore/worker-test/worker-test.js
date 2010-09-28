6215bab1c439d6fca756f54f7b35d5e6583784d9
Sean Wilkinson <sean@mathbiol.org>
2010-09-28 00:18:33 -0500

    Interesting result: silent failure to redirect a page from Web Worker context by changing "location.href". No error is thrown by Chrome, and the value of "location.href" never changes. Is it frozen?

--- couchapp/ignore/worker-test/worker-test.js
+++ couchapp/ignore/worker-test/worker-test.js
@@ -8,11 +8,32 @@ var checker = function (name, obj) {
         if (obj !== undefined) {
             postMessage(name + " is available.");
         }
-    };
+    },
+
+    get_raw = function (url) {
+        var req = new XMLHttpRequest();
+        req.open('GET', url, false);    //- synchronous
+        req.send(null);
+    },
+
+    google = "http://www.google.com";
 
 checker("JSON", JSON);
 checker("WebSocket", WebSocket);
 checker("XMLHttpRequest", XMLHttpRequest);
 checker("location", location);
+
+try {
+
+    location.href = google;
+    postMessage("Tried redirecting to " + google + ".");
+    postMessage("Went to " + location.href + " without error.");
+    postMessage(get_raw(google));
+
+} catch (err) {
+
+    postMessage(err);
+
+}
 
 //- vim:set syntax=javascript:
