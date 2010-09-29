e4b2c9eff3a6b401003eaa0fc2a91103d0a1bf5c
Sean Wilkinson <sean@mathbiol.org>
2010-09-28 20:10:13 -0500

    Added hooks to indicate author, time, and volunteer. Time obviously comes in many varieties, but this is an indication that extending the current design is very easy.

--- couchapp/_attachments/bee.js
+++ couchapp/_attachments/bee.js
@@ -3,26 +3,31 @@
 //- bee.js ~~
 //  This is where the magic happens. The computational nodes poll a URL where
 //  tasks needing to be accomplished are posted by a CouchDB filter rule. The
-//  node then executes the first task listed and uploads its results. The task
-//  itself runs within a Web Worker context, which increases both security and
-//  performance for all parties involved. The FAQ lists which browsers work ...
+//  node then executes the first task listed and uploads its results. Each task
+//  itself runs within a fresh Web Worker context (a special JS sandbox) and
+//  then exits immediately. There is a "setTimeout loop" that creates another
+//  worker when this one closes.
 //                                                          ~~ SRW, 27 Sep 2010
 
 importScripts("Q.js");
 
-(function (queue) {
-    var fetch = function () {
-        var changes = Q.io.read(queue),
-            latest = {},
-            results = changes.results || [];
-        if (results.length > 0) {
-            latest = Q.io.read(results[0].id);
-            latest.results = Q.run(latest.code);
-            Q.io.write(latest);
-        }
-        setTimeout(fetch, 1000);
-    };
-    setTimeout(fetch, 1000);
-}("_changes?filter=quanah/code"));
+Q.merge(Q, this);                       //- gives Jonas the syntax he wanted :)
+
+var changes = Q.couch.read("_changes?filter=quanah/code"),
+    latest = {},
+    results = changes.results || [];
+
+if (results.length > 0) {
+
+    latest = Q.couch.read(results[0].id);
+    latest.volunteer = Q.whoami();
+    latest.results = Q.run(latest.code);
+    Q.couch.write(latest);              //- security hole for server?
+
+}
+
+var t = 1000;                           //- milliseconds till Worker closes
+postMessage(t);
+setTimeout(close, t);
 
 //- vim:set syntax=javascript:
--- couchapp/ignore/worker-test/worker-test.js
+++ couchapp/ignore/worker-test/worker-test.js
@@ -4,36 +4,50 @@
 //  A test script to see what technologies are available to a Web Worker.
 //                                                          ~~ SRW, 25 Sep 2010
 
-var checker = function (name, obj) {
+var check_for = function (name, obj) {
         if (obj !== undefined) {
             postMessage(name + " is available.");
+        } else {
+            postMessage(name + " is NOT available.");
         }
     },
 
-    get_raw = function (url) {
-        var req = new XMLHttpRequest();
-        req.open('GET', url, false);    //- synchronous
-        req.send(null);
-    },
+    try_redirect = function () {
+        var get_raw = function (url) {
+                var req = new XMLHttpRequest();
+                req.open('GET', url, false);    //- synchronous
+                req.send(null);
+            },  
 
-    google = "http://www.google.com";
+            google = "http://www.google.com";
 
-checker("JSON", JSON);
-checker("WebSocket", WebSocket);
-checker("XMLHttpRequest", XMLHttpRequest);
-checker("location", location);
+        try {
 
-try {
+            location.href = google;
+            postMessage("Tried redirecting to " + google + ".");
+            postMessage("Went to " + location.href + " without error.");
+            postMessage(get_raw(google));
 
-    location.href = google;
-    postMessage("Tried redirecting to " + google + ".");
-    postMessage("Went to " + location.href + " without error.");
-    postMessage(get_raw(google));
+        } catch (err) {
+            
+            postMessage(err);
+        
+        }
 
-} catch (err) {
+    };
 
-    postMessage(err);
+//- TESTS
 
-}
+check_for("JSON", this.JSON);
+check_for("location", this.location);
+check_for("navigator", this.navigator);
+check_for("WebSocket", this.WebSocket);
+check_for("Worker", this.Worker);
+check_for("XMLHttpRequest", this.XMLHttpRequest);
+//try_redirect();
+
+postMessage("closing");
+setTimeout(close, 1000);
+
 
 //- vim:set syntax=javascript:
