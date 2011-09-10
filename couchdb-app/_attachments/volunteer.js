6c5c2773da9f2f517c15f63dc349cf297fd26a35
sean@mathbiol.org <sean@mathbiol.org>
2011-09-10 00:23:05 -0500

    Revisioning issues aside, the volunteer mechanism is working quite nicely -- the JSONP is a good move :-)

--- couchdb-app/_attachments/volunteer.js
+++ couchdb-app/_attachments/volunteer.js
@@ -2,11 +2,37 @@
 
 //- volunteer.js ~~
 //
-//  Yes, it's hard-coded, but I'll deal with that later :-P
+//  Yes, it's hard-coded, but I'll deal with that later. It's working pretty
+//  well, but I'm tired of mixing hard code and copy/paste -- the revision
+//  "gotchas" in CouchDB are pretty inconvenient when you have to handle them
+//  manually. I'm going to bed.
 //
-//                                                      ~~ (c) SRW, 09 Sep 2011
+//                                                      ~~ (c) SRW, 10 Sep 2011
 
-var bookmarks, cache;
+var ajax$put, bookmarks, cache, isFunction;
+
+ajax$put = function (url, data, callback) {
+    var req = new XMLHttpRequest();
+    req.onreadystatechange = function () {
+        var err, txt;
+        err = null;
+        txt = null;
+        if (req.readyState === 4) {
+            if (req.status === 201) {
+                txt = req.responseText;
+            } else {
+                err = new Error('Could not PUT to "' + url + '".');
+            }
+            if (isFunction(callback) === true) {
+                callback(err, txt);
+            }
+        }
+    };
+    req.open("PUT", url, true);
+    req.setRequestHeader("Content-type", "application/json");
+    req.send(data);
+    return req;
+};
 
 if (location.port === "") {
  // This happens if we are actually using the rewrite rules I created
@@ -39,16 +65,24 @@ cache = {
     },
     main: function (x) {
         cache.main = eval(x.content);
+        cache.main$original = x;
     },
     queue: function (x) {
         cache.queue = x.hasOwnProperty("results") ? x.results : [];
+        cache.queue$original = x;
     },
     results: function (x) {
         cache.results = x.content;
+        cache.results$original = x;
     },
     task: function (x) {
         cache.task = x.content;
+        cache.task$original = x;
     }
+};
+
+isFunction = function (f) {
+    return ((typeof f === 'function') && (f instanceof Function));
 };
 
 importScripts(
@@ -59,11 +93,26 @@ importScripts(
 if (cache.queue.length > 0) {
 
     importScripts(
-        (bookmarks.db + cache.queue[cache.queue.length - 1].id +
-            "?callback=cache.task")
+        (bookmarks.db + cache.queue[0].id + "?callback=cache.task")
     );
 
- // (NOTE: Status change from "waiting" to "running" will go here.)
+    cache.task$original.content.status = "running";
+
+    ajax$put(bookmarks.db + cache.queue[0].id,
+        JSON.stringify(cache.task$original),
+        function (err, txt) {
+            var response;
+            if (err !== null) {
+                postMessage(err);
+            } else {
+                response = JSON.parse(txt);
+                if (response.ok) {
+                    cache.task$original._rev = response.rev;
+                    postMessage(txt);
+                }
+            }
+        }
+    );
 
     importScripts(
         (cache.task.argv + "?callback=cache.argv"),
@@ -71,13 +120,44 @@ if (cache.queue.length > 0) {
         (cache.task.results + "?callback=cache.results")
     );
 
-    cache.results = cache.main(cache.argv);
+    try {
+        cache.results = cache.main(cache.argv);
+    } catch (err) {
+        cache.results = err;
+    } finally {
+        cache.results$original.content = cache.results;
+    }
 
- // (NOTE: Status change from "running" to "done" will go here.)
+    ajax$put(bookmarks.db + cache.queue[0].id,
+        JSON.stringify(cache.task$original),
+        function (err, txt) {
+            var response;
+            if (err !== null) {
+                postMessage(err);
+            } else {
+                response = JSON.parse(txt);
+                if (response.ok) {
+                    cache.task$original._rev = response.rev;
+                    postMessage(txt);
+                }
+            }
+        }
+    );
 
- // (NOTE: Upload of results back to CouchDB will go here.)
+    ajax$put(cache.task$original.content.results,
+        JSON.stringify(cache.results$original),
+        function (err, txt) {
+            if (err !== null) {
+                postMessage(err);
+            } else {
+                postMessage(txt);
+            }
+        }
+    );
 
-    postMessage(cache.results);
+} else {
+
+    postMessage("Nothing to do ...");
 
 }
 
