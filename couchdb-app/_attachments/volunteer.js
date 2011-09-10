e5c6aa184de8ea7ab93e45fb53e1ee48668f4e4b
Sean Wilkinson <sean@mathbiol.org>
2011-09-09 20:29:44 -0500

    Ready to write the "volunteer.js" worker script, but first I need dinner.

---
+++ couchdb-app/_attachments/volunteer.js
@@ -0,0 +1,12 @@
+//- JavaScript source code
+
+//- volunteer.js ~~
+//                                                      ~~ (c) SRW, 09 Sep 2011
+
+importScripts(
+    "method-Q.js"
+);
+
+postMessage("Volunteer script is working correctly.");
+
+//- vim:set syntax=javascript:
