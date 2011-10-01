0ec037109fb5260f1fff4e7e841613ac8355e3d7
sean@mathbiol.org <sean@mathbiol.org>
2011-10-01 12:02:30 -0500

    Closer still, but not quite to my liking yet ;-)

--- couchdb-app/_attachments/method-Q.js
+++ couchdb-app/_attachments/method-Q.js
@@ -2,9 +2,17 @@
 
 //- method-Q.js ~~
 //
-//  This version is not yet complete ...
+//  This version is not yet complete, so the summary here is more of a listing
+//  of current design decisions:
+//  -   http://localhost/?developer=false&developer=true
 //
-//                                                      ~~ (c) SRW, 30 Sep 2011
+//  NEED TO FIX:
+//  -   Redefine "uuid" so that it _never_ needs to block ...
+//  -   Full consistency in QuanahVar properties
+//  -   Finish the "onready" handler ...
+//  -   Test for presence of native JSON and load it if missing?
+//
+//                                                      ~~ (c) SRW, 01 Oct 2011
 
 (function (global) {
     'use strict';
@@ -20,27 +28,75 @@
         throw new Error('"Method Q" is currently browser-only.');
     }
 
-    if (global.hasOwnProperty('JSON') === false) {
-     // Should we load "json2.js" or just throw an error?
-    }
-
  // Private declarations
 
-    var argv, bookmarks, define, isFunction, read, uuid, write;
+    var argv, bookmarks, countdown, define, isFunction, read, uuid, write;
 
  // Private definitions
 
-    argv = {
-     // These "placeholder properties" will be filled later in "Invocations".
-        developer:  null,
-        volunteer:  null
-    };
+    argv = {};
+    bookmarks = {};
 
-    bookmarks = {
-     // These "placeholder properties" will be filled later in "Invocations".
-        db:         null,
-        uuids:      function (n) {}
-    };
+    (function () {
+     // This anonymous closure is based in part on parseUri 1.2.2 by Steven
+     // Levithan (stevenlevithan.com, MIT License). It treats 'location.search'
+     // as a set of ampersand-separated Boolean key=value parameters whose
+     // keys are valid JS identifiers and whose values are either "true" or
+     // "false" (without quotes). The function accepts an object whose own
+     // properties will be used to override flags that are already present.
+        var key, opts, parseURI, results, uri;
+        opts = {
+            key: [
+                'source', 'protocol', 'authority', 'userInfo', 'user',
+                'password', 'host', 'port', 'relative', 'path', 'directory',
+                'file', 'query', 'anchor'
+            ],
+            parser: new RegExp('^(?:([^:\\/?#]+):)?(?:\\/\\/((?:(([^:@' +
+                ']*)(?::([^:@]*))?)?@)?([^:\\/?#]*)(?::(\\d*))?))?((('  +
+                '(?:[^?#\\/]*\\/)*)([^?#]*))(?:\\?([^#]*))?(?:#(.*))?)'),
+            q: {
+                name:   'flags',
+                parser: /(?:^|&)([^&=]*)=?([^&]*)/g
+            }
+        };
+        parseURI = function (str) {
+            var i, m, uri;
+            i = 14;
+            m = opts.parser.exec(str);
+            uri = {};
+            for (i = 14; i > 0; i -= 1) {
+                uri[opts.key[i]] = m[i] || '';
+            }
+            uri[opts.q.name] = {};
+            uri[opts.key[12]].replace(opts.q.parser, function ($0, $1, $2) {
+                if ($1) {
+                    uri[opts.q.name][$1] = ($2 !== 'false') ? true : false;
+                }
+            });
+            return uri;
+        };
+        uri = parseURI(global.location.href);
+     // First, let's compute the "command-line arguments" :-)
+        for (key in uri.flags) {
+            if (uri.flags.hasOwnProperty(key)) {
+                argv[key] = uri.flags[key];
+            }
+        }
+     // Now, let's define some bookmarks we will need during execution.
+        bookmarks.db = uri.protocol + '://' + uri.authority + '/db/';
+        bookmarks.uuids = function (n) {
+            return uri.protocol + '://' + uri.authority +
+                '/_uuids?count=' + parseInt(n >> 0);
+        };
+    }());
+
+    countdown = function (n, callback) {
+        var total = (n >> 0);
+        return function () {
+            total -= 1;
+            return (total === 0) ? callback() : null;
+        };
+    }; 
 
     define = function (obj, key, params) {
         if (typeof Object.defineProperty === 'function') {
@@ -109,7 +165,7 @@
      // necessary, but only then will Quanah be free of blocking calls ...
         var cache = [];
         uuid = function () {
-            var req, uuids;
+            var req, uuids, x;
             if (cache.length < 500) {
                 read(bookmarks.uuids(1000), function (err, txt) {
                     if (err !== null) {
@@ -120,6 +176,8 @@
                 });
                 if (cache.length === 0) {
                  // If we're totally empty, we will block until more arrive.
+                 // Alternatively, though, we could just one of our own real
+                 // quick; if it fails, we could try again recursively ... ?
                     req = new XMLHttpRequest();
                     req.open('GET', bookmarks.uuids(1000), false);
                     req.send();
@@ -157,140 +215,221 @@
 
  // Private constructors
 
-    function QuanahFxn(obj) {           //- PLACEHOLDER
-        var that;
+    function QuanahFxn(obj) {
+        obj = ((typeof obj === 'object') && (obj !== null)) ? obj : {};
+        var id, that;
+        id = obj.hasOwnProperty('_id') ? obj._id : uuid();
         that = this;
+        that.meta = {
+            _id:    id,
+            _rev:   obj.hasOwnProperty('_rev') ? obj._rev : null,
+            type:   'QuanahFxn',
+            url:    bookmarks.db + id
+        };
+        that.content = obj.hasOwnProperty('content') ? obj.content : function () {};
         return that;
     }
 
-    function QuanahTask(obj) {          //- PLACEHOLDER
-        var that;
+    function QuanahTask(obj) {
+        obj = ((typeof obj === 'object') && (obj !== null)) ? obj : {};
+        var id, that;
+        id = obj.hasOwnProperty('_id') ? obj._id : uuid();
         that = this;
+        that.meta = {
+            _id:    id,
+            _rev:   obj.hasOwnProperty('_rev') ? obj._rev : null,
+            type:   'QuanahTask',
+            url:    bookmarks.db + id
+        };
+        that.content = obj.hasOwnProperty('content') ? obj.content : function () {};
         return that;
     }
 
-    function QuanahVar(obj) {           //- PLACEHOLDER
-        var that;
+    function QuanahVar(obj) {
+        obj = ((typeof obj === 'object') && (obj !== null)) ? obj : {};
+        var id, that;
+        id = obj.hasOwnProperty('_id') ? obj._id : uuid();
         that = this;
+        that.meta = {
+            _id:    id,
+            _rev:   obj.hasOwnProperty('_rev') ? obj._rev : null,
+            type:   'QuanahVar',
+            url:    bookmarks.db + id
+        };
+        that.content = obj.hasOwnProperty('content') ? obj.content : function () {};
         return that;
     }
 
-    QuanahFxn.prototype = new QuanahVar();
+    QuanahFxn.prototype = new QuanahVar(null);
 
-    QuanahTask.prototype = new QuanahVar();
+    QuanahTask.prototype = new QuanahVar(null);
 
     QuanahVar.prototype.pull = function () {
-     // (placeholder)
+        var that = this;
+        that.onready = function (data, exit) {
+            read(that.meta.url, function (err, txt) {
+                var key, response;
+                if (err === null) {
+                 // We can test for the presence of this property to see if
+                 // the variable has been initialized on CouchDB yet or not.
+                    response = JSON.parse(txt);
+                    that.meta._rev = response._rev;
+                    exit.success(response.content);
+                } else {
+                    exit.failure(err);
+                }
+            });
+        };
+        return that;
     };
 
     QuanahVar.prototype.push = function () {
-     // (placeholder)
+        var that = this;
+        that.onready = function (content, exit) {
+            var obj;
+            obj = {
+                _id:        that.meta._id,
+                content:    content,
+                type:       that.meta.type
+            };
+            if (that.meta._rev !== null) {
+                obj._rev = that.meta._rev;
+            }
+            write(that.meta.url, JSON.stringify(obj), function (err, txt) {
+                var response;
+                if (err === null) {
+                 // We can test for the presence of this property to see if
+                 // the variable has been initialized on CouchDB yet or not.
+                    response = JSON.parse(txt);
+                    if (response.ok === true) {
+                        that.meta._rev = response.rev;
+                        exit.success(content);
+                    } else {
+                        exit.failure(response);
+                    }
+                } else {
+                    exit.failure(err);
+                }
+            });
+        };
+        return that;
     };
 
     define(QuanahVar.prototype, 'onready', {
         configurable: false,
         enumerable: true,
         get: function () {
-         // (placeholder)
+         // FIX THIS LATER!!!
+            console.log('(getter invoked)');
+            //return (stack.length > 0) ? stack[0] : null;
         },
         set: function (f) {
-         // (placeholder)
+            var egress, ready, revive, stack, that;
+            egress = function () {
+                return {
+                    failure: function (x) {
+                        that.content = x;
+                        console.error(that);    //- REMOVE THIS LATER!
+                    },
+                    success: function (x) {
+                        that.content = x;
+                        ready = true;
+                        revive();
+                    }
+                };
+            };
+            ready = true;
+            revive = function () {
+                var f;
+                if (ready === true) {
+                    ready = false;
+                    f = stack.shift();
+                    if (f === undefined) {
+                        ready = true;
+                    } else {
+                        f.call(that, that.content, egress());
+                    }
+                }
+            };
+            stack = [];
+            that = this;
+            define(that, "onready", {
+                configurable: false,
+                enumerable: true,
+                get: function () {
+                    return (stack.length > 0) ? stack[0] : null;
+                },
+                set: function (f) {
+                    if (isFunction(f) === false) {
+                        throw new Error('"onready" expects a function.');
+                    }
+                    stack.push(f);
+                    revive();
+                }
+            });
+            return (that.onready = f);
         }
     });
 
  // Global definitions
 
     Object.prototype.Q = function (key, func) {
-        return this;                    //- PLACEHOLDER DEFINITION
+        var f, x, y, z;
+        f = (new QuanahFxn({content: func})).push();
+        x = (new QuanahVar({content: this})).push();
+        y = (new QuanahVar({content: null})).push();
+        z = (new QuanahTask({content: null})).push();
+        z.onready = function (z, exit) {
+            var count;
+            count = countdown(3, function () {
+                z.status = "waiting";
+                z.push();
+                exit.success(z);
+            });
+            f.onready = function (content, exit) {
+                try {
+                    z.main = f.meta.url;
+                    exit.success(content);
+                    count();
+                } catch (err) {
+                    exit.failure(err);
+                }
+            };
+            x.onready = function (content, exit) {
+                try {
+                    z.argv = x.meta.url;
+                    exit.success(content);
+                    count();
+                } catch (err) {
+                    exit.failure(err);
+                }
+            };
+            y.onready = function (content, exit) {
+                try {
+                    z.results = y.meta.url;
+                    exit.success(content);
+                    count();
+                } catch (err) {
+                    exit.failure(err);
+                }
+            };
+        };
+        return z;
     };
 
  // Invocations
 
-    (function () {
-     // This anonymous closure is based in part on parseUri 1.2.2 by Steven
-     // Levithan (stevenlevithan.com, MIT License). It treats 'location.search'
-     // as a set of ampersand-separated Boolean key=value parameters whose
-     // keys are valid JS identifiers and whose values are either "true" or
-     // "false" (without quotes). The function accepts an object whose own
-     // properties will be used to override flags that are already present.
-        var key, opts, parseURI, results, uri;
-        opts = {
-            key: [
-                'source', 'protocol', 'authority', 'userInfo', 'user',
-                'password', 'host', 'port', 'relative', 'path', 'directory',
-                'file', 'query', 'anchor'
-            ],
-            parser: new RegExp('^(?:([^:\\/?#]+):)?(?:\\/\\/((?:(([^:@' +
-                ']*)(?::([^:@]*))?)?@)?([^:\\/?#]*)(?::(\\d*))?))?((('  +
-                '(?:[^?#\\/]*\\/)*)([^?#]*))(?:\\?([^#]*))?(?:#(.*))?)'),
-            q: {
-                name:   'flags',
-                parser: /(?:^|&)([^&=]*)=?([^&]*)/g
-            }
-        };
-        parseURI = function (str) {
-            var i, m, uri;
-            i = 14;
-            m = opts.parser.exec(str);
-            uri = {};
-            for (i = 14; i > 0; i -= 1) {
-                uri[opts.key[i]] = m[i] || '';
-            }
-            uri[opts.q.name] = {};
-            uri[opts.key[12]].replace(opts.q.parser, function ($0, $1, $2) {
-                if ($1) {
-                    uri[opts.q.name][$1] = ($2 !== 'false') ? true : false;
-                }
-            });
-            return uri;
-        };
-        uri = parseURI(global.location.href);
-     // First, let's compute the "command-line arguments" :-)
-        for (key in uri.flags) {
-            if (uri.flags.hasOwnProperty(key)) {
-                argv[key] = uri.flags[key];
-            }
-        }
-     // Now, let's define some bookmarks we will need during execution.
-        bookmarks.db = uri.protocol + '://' + uri.authority + '/db/';
-        bookmarks.uuids = function (n) {
-            return uri.protocol + '://' + uri.authority +
-                '/_uuids?count=' + parseInt(n >> 0);
-        };
-    }());
-
     if (argv.developer === true) {
         (function developer() {
-         // This "disposable closure" is only a demo -- remove it later!
-            var x, y;
-            x = [1, 2, 3, 4, 5];
-            y = x.Q(function (x) {
-                var i, n, y;
-                n = x.length;
-                y = [];
-                for (i = 0; i < n; i += 1) {
-                 // Yes, this is "map" pattern, but it's just a demo.
-                    y[i] = 3 * x[i];
-                }
-                return y;
-            });
-            y.onready = function (data, exit) {
-                console.log(data);
-                exit.success('(printed y)');
-            };
+            console.log('--- DEVELOPER MODE ENABLED ---');
         }());
     }
 
     if (argv.volunteer === true) {
         (function volunteer() {
-         // This "disposable closure" contains the complete Web Worker script
-         // for the volunteers' machines :-)
-            var bee, read, puts, write; //- PLACEHOLDER DEFINITION ...
+            console.log('--- VOLUNTEER MODE ENABLED ---');
         }());
     }
-
-    console.log(argv);
-    console.log(bookmarks, bookmarks.uuids(10));
 
 }(function (outer) {
     'use strict';
