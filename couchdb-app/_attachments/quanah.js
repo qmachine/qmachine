0ee36f2e84863c4d828338514c10bb2cfd64b707
Sean Wilkinson <sean@mathbiol.org>
2011-10-20 15:31:30 -0500

    Began porting "Method Q" ...

--- couchdb-app/_attachments/quanah.js
+++ couchdb-app/_attachments/quanah.js
@@ -1,6 +1,11 @@
 //- JavaScript source code
 
 //- quanah.js ~~
+//
+//  This program defines Quanah in terms of Web Chassis. It depends on global
+//  variables "chassis" and "RAINMAN" (the latter indirectly), but ultimately
+//  I plan to encapsulate all parts into a single file someday ...
+//
 //                                                      ~~ (c) SRW, 19 Oct 2011
 
 chassis(function (q, global) {
@@ -15,22 +20,86 @@ chassis(function (q, global) {
         return;
     }
 
- // Declarations
+ // Private declarations
 
-    var sync, x;
+    var countdown, isFunction, sync;
 
- // Definitions
+ // Private definitions
+
+    countdown = function (n, callback) {
+        var total = parseInt(Math.abs(n));
+        return function () {
+            total -= 1;
+            if (total === 0) {
+                callback.apply(this, arguments);
+            }
+        };
+    };
+
+    isFunction = function (f) {
+        return ((typeof f === 'function') && (f instanceof Function));
+    };
 
     sync = q.fs$sync;
 
+ // Global definitions
+
+    Object.prototype.Q = function (func) {
+        var count, f, x, y, task;
+        if (isFunction(func)) {
+            count = countdown(3, function () {
+                task.onready = function (val, exit) {
+                    val.f = f.key;
+                    val.x = x.key;
+                    val.y = y.key;
+                    val.status = 'waiting';
+                    exit.success(val);
+                };
+                sync(task);
+            });
+            f = sync({val: func});
+            x = sync((this instanceof f.constructor) ? this : {val: this});
+            y = sync({val: null});
+            task = sync({
+                val: {
+                    f:      null,
+                    x:      null,
+                    y:      null,
+                    status: 'initializing'
+                }
+            });
+            f.onready = x.onready = y.onready = function (val, exit) {
+                count();
+                exit.success(val);
+            };
+            y.onready = function (val, exit) {
+             // NOTE: See polling mechanism at http://goo.gl/TwYXA .
+            };
+        } else {
+            throw new Error('Method Q expects a function as its argument.');
+        }
+    };
+
  // Invocations
 
-    x = sync({key: 'lala'});
+    if (q.argv.developer === true) {
+        (function developer_startup() {
+         // This is just a demonstration script, actually ...
+            console.log('--- Developer mode ---');
+            var x;
+            x = sync({key: 'lala'});
+            x.onready = function (val, exit) {
+                console.log(val);
+                exit.success(val);
+            };
+        }());
+    }
 
-    x.onready = function (val, exit) {
-        console.log(val);
-        exit.success(val);
-    };
+    if (q.argv.volunteer === true) {
+        (function volunteer_startup() {
+            console.log('--- Volunteer mode ---');
+        }());
+    }
 
 });
 
