6512bbb367a55440556432c4b2d73d73cb41734d
Sean Wilkinson <sean@mathbiol.org>
2011-10-21 18:57:25 -0500

    This branch is now working!

--- couchdb-app/views/tasks/map.js
+++ couchdb-app/views/tasks/map.js
@@ -19,15 +19,13 @@ function (doc) {
     var v, y;
     if (doc.hasOwnProperty('val') === true) {
         v = doc.val;
-        y = v.hasOwnProperty('f') && v.hasOwnProperty('x') &&
-            v.hasOwnProperty('y') && v.hasOwnProperty('status');
+        y = v.hasOwnProperty('f')       &&
+            v.hasOwnProperty('x')       &&
+            v.hasOwnProperty('y')       &&
+            v.hasOwnProperty('status')  &&
+            v.hasOwnProperty('token');
         if (y === true) {
-            if (v.hasOwnProperty('token')) {
-             // Here is preliminary token support, per Jonas's request.
-                emit([v.status, v.token], {f: v.f, x: v.x, y: v.y});
-            } else {
-                emit(v.status, {f: v.f, x: v.x, y: v.y});
-            }
+            emit(v.status, v.token);
         }
     }
 };
