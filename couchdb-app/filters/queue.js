d945055a1c79492597ab8ef952938388e1e11ad7
Sean Wilkinson <sean@mathbiol.org>
2011-10-26 08:46:29 -0500

    Tried to generalize the filter -- see comments

--- couchdb-app/filters/queue.js
+++ couchdb-app/filters/queue.js
@@ -20,6 +20,39 @@ function (doc, req) {
             (req.query.hasOwnProperty('token') ?
                 (doc.val.token === req.query.token) : true));
 
+ // The next part is an attempt to construct a generalized query mechanism for
+ // arbitrary terms -- it doesn't work yet ...
+ //
+ //                                                     ~~ (c) SRW, 26 Oct 2011
+
+ /*
+    var isTask, matchesQuery;
+
+    isTask = function (doc) {
+        return ((doc.hasOwnProperty('val'))         &&
+                (doc.val !== null)                  &&
+                (doc.val !== undefined)             &&
+                (doc.val.hasOwnProperty('f'))       &&
+                (doc.val.hasOwnProperty('x'))       &&
+                (doc.val.hasOwnProperty('y'))       &&
+                (doc.val.hasOwnProperty('status'))) ;
+    };
+
+    matchesQuery = function (doc, req) {
+        var key, query;
+        query = (req.hasOwnProperty('query')) ? req.query : {};
+        for (key in query) {
+            if (query.hasOwnProperty(key) && doc.hasOwnProperty(key)) {
+                if (query[key] !== doc[key]) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    };
+
+    return isTask(doc) && matchesQuery(doc, req);
+ */
 }
 
 //- vim:set syntax=javascript:
