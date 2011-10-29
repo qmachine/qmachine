f6a15a2f4bc480d944cae661bb5d6362403cb506
Sean Wilkinson <sean@mathbiol.org>
2011-10-28 20:07:24 -0500

    Generalized the filter function! :-)

--- couchdb-app/filters/queue.js
+++ couchdb-app/filters/queue.js
@@ -1,31 +1,11 @@
 //- JavaScript source code
 
 //- queue.js ~~
-//                                                      ~~ (c) SRW, 21 Oct 2011
+//                                                      ~~ (c) SRW, 28 Oct 2011
 
 function (doc, req) {
     'use strict';
 
-    return ((doc.hasOwnProperty('val'))                         &&
-            (doc.val !== null)                                  &&
-            (doc.val !== undefined)                             &&
-            (doc.val.hasOwnProperty('f'))                       &&
-            (doc.val.hasOwnProperty('x'))                       &&
-            (doc.val.hasOwnProperty('y'))                       &&
-            (doc.val.hasOwnProperty('status'))                  &&
-            (doc.val.hasOwnProperty('token'))                   &&
-            (req.hasOwnProperty('query'))                       &&
-            (req.query.hasOwnProperty('status') ?
-                (doc.val.status === req.query.status) : true)   &&
-            (req.query.hasOwnProperty('token') ?
-                (doc.val.token === req.query.token) : true));
-
- // The next part is an attempt to construct a generalized query mechanism for
- // arbitrary terms -- it doesn't work yet ...
- //
- //                                                     ~~ (c) SRW, 26 Oct 2011
-
- /*
     var isTask, matchesQuery;
 
     isTask = function (doc) {
@@ -39,11 +19,16 @@ function (doc, req) {
     };
 
     matchesQuery = function (doc, req) {
-        var key, query;
-        query = (req.hasOwnProperty('query')) ? req.query : {};
-        for (key in query) {
-            if (query.hasOwnProperty(key) && doc.hasOwnProperty(key)) {
-                if (query[key] !== doc[key]) {
+        var key, q, v;
+        q = (req.hasOwnProperty('query')) ? req.query : {};
+        v = doc.val;
+        for (key in q) {
+            if (q.hasOwnProperty(key) && v.hasOwnProperty(key)) {
+             // NOTE: This ignores keys that don't exist on the document so
+             // that CouchDB's special filter keywords won't interfere. Then,
+             // you can still specify "limit=10", for example, to shorten a
+             // list of results without blacklisting the keywords explicitly.
+                if (q[key] !== v[key]) {
                     return false;
                 }
             }
@@ -52,7 +37,7 @@ function (doc, req) {
     };
 
     return isTask(doc) && matchesQuery(doc, req);
- */
+
 }
 
 //- vim:set syntax=javascript:
