1f40242a42cdd70e94f5186dbc0f30beb4c1d232
Sean Wilkinson <sean@mathbiol.org>
2011-10-31 16:41:49 -0500

    Removed subfunctions from the filter function

--- couchdb-app/filters/queue.js
+++ couchdb-app/filters/queue.js
@@ -1,43 +1,45 @@
 //- JavaScript source code
 
 //- queue.js ~~
-//                                                      ~~ (c) SRW, 28 Oct 2011
+//
+//  This file contains a generalized solution to querying JSON documents'
+//  properties according to known values. Additionally, using the "_changes"
+//  API leaves an easy way room to add authentication on a per-request basis,
+//  if ever such measures should be needed for access control :-)
+//
+//                                                      ~~ (c) SRW, 31 Oct 2011
 
 function (doc, req) {
     'use strict';
 
-    var isTask, matchesQuery;
+    var flag, key, q, v;
 
-    isTask = function (doc) {
-        return ((doc.hasOwnProperty('val'))         &&
-                (doc.val !== null)                  &&
-                (doc.val !== undefined)             &&
-                (doc.val.hasOwnProperty('f'))       &&
-                (doc.val.hasOwnProperty('x'))       &&
-                (doc.val.hasOwnProperty('y'))       &&
-                (doc.val.hasOwnProperty('status'))) ;
-    };
+    if (doc.hasOwnProperty('val')) {
+        v = ((doc.val !== null) && (doc.val !== undefined)) ? doc.val : {};
+        flag = ((v.hasOwnProperty('f')) && (v.hasOwnProperty('x')) &&
+                (v.hasOwnProperty('y')) && (v.hasOwnProperty('status')));
+        if (flag !== true) {
+            return false;
+        }
+    } else {
+        return false;
+    }
 
-    matchesQuery = function (doc, req) {
-        var key, q, v;
-        q = (req.hasOwnProperty('query')) ? req.query : {};
-        v = doc.val;
-        for (key in q) {
-            if (q.hasOwnProperty(key) && v.hasOwnProperty(key)) {
-             // NOTE: This ignores keys that don't exist on the document so
-             // that CouchDB's special filter keywords won't interfere. Then,
-             // you can still specify "limit=10" or "since=100", for example,
-             // to subset results without blacklisting keywords explicitly :-)
-                if (q[key] !== v[key]) {
-                    return false;
-                }
+    q = (req.hasOwnProperty('query')) ? req.query : {};
+
+    for (key in q) {
+        if (q.hasOwnProperty(key) && v.hasOwnProperty(key)) {
+         // NOTE: This ignores keys that don't exist on the document so
+         // that CouchDB's special filter keywords won't interfere. Then,
+         // you can still specify "limit=10" or "since=100", for example,
+         // to subset results without blacklisting keywords explicitly :-)
+            if (q[key] !== v[key]) {
+                return false;
             }
         }
-        return true;
-    };
+    }
 
-    return isTask(doc) && matchesQuery(doc, req);
-
+    return true;
 }
 
 //- vim:set syntax=javascript:
