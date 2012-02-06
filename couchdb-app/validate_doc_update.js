f385af720f5ff0afab1e2b74a7c6796981744aa8
Sean Wilkinson <sean@mathbiol.org>
2012-02-06 11:20:06 -0600

    After developing in my private repository for the last few months, I created a branch for "qmachine.org" as an example use case of Quanah and for the first paper. It still needs to be cleaned up, but the main idea here is that Qmachine is the web service, but Quanah is a library for concurrent programming in JavaScript

--- couchdb-app/validate_doc_update.js
+++ couchdb-app/validate_doc_update.js
@@ -1,16 +1,30 @@
 //- JavaScript source code
 
 //- validate_doc_update.js ~~
-//
-//  Currently, this function allows all document updates to the database,
-//  regardless of content or user roles. I'll work on it soon.
-//
-//                                                      ~~ (c) SRW, 15 Nov 2011
+//                                                      ~~ (c) SRW, 06 Feb 2012
 
-function (newDoc, savedDoc, userCtx) {
+function (newDoc, oldDoc, userCtx) {
     'use strict';
 
- // (placeholder)
+ // Pragmas
+
+    /*jslint indent: 4, maxlen: 80, nomen: true */
+
+ // This rule rejects updates for any document whose ID isn't a hexadecimal
+ // UUID of length 32.
+
+    if ((/^[0-9a-f]{32}$/).test(newDoc._id) === false) {
+        throw {
+            forbidden: 'Invalid document id (' + newDoc._id + ').'
+        };
+    }
+
+ // It would also be nice to capture a timestamp, but because that involves an
+ // 'update' function, we'll save that for another day.
+
+ // That's all, folks!
+
+    return;
 
 }
 
