d4f6e13e97e9d0f972479d3d94151e4c0ff29172
Sean Wilkinson <sean@mathbiol.org>
2012-03-07 09:17:55 -0600

    Updated Quanah (now sports "map", "ply", "reduce")

--- node-sqlite3/quanah.js
+++ node-sqlite3/quanah.js
@@ -17,20 +17,21 @@
 //
 //  To-do list:
 //
-//  -   add support for 'x.onready = f' when 'f' is an avar
+//  -   enable 'when(x).isready = f' for avars f
 //  -   remove type-checks in user-unreachable functions where appropriate
 //  -   replace 'throw' statements with 'fail' statements for robustness
 //  -   rewrite 'onready' assignments as 'comm' invocations (optional)
+//  -   rewrite 'remote_call' in terms of a single avar to be like 'volunteer'
 //
 //  Open questions:
 //
 //  -   Can Quanah return a remotely distributed memoized function?
 //  -   Could Quanah actually support ActionScript?
-//  -   Can I prevent users' own JSLINT pragmas from circumventing 'isClosed'?
+//  -   Can users' own JSLINT pragmas circumvent the 'isClosed' function?
 //  -   Is Quanah a kernel?
 //      -   If so, is it "re-entrant"? See http://goo.gl/985r.
 //
-//                                                      ~~ (c) SRW, 03 Mar 2012
+//                                                      ~~ (c) SRW, 05 Mar 2012
 
 (function (global) {
     'use strict';
@@ -53,9 +54,9 @@
  // Declarations
 
     var atob, AVar, avar, btoa, comm, defineProperty, deserialize,
-        init, isArrayLike, isClosed, isFunction, local_call, ply,
-        puts, remote_call, revive, secret, serialize, stack, sys,
-        update_local, update_remote, uuid, volunteer, when;
+        dmap, dply, dreduce, init, isArrayLike, isClosed, isFunction,
+        local_call, ply, puts, remote_call, revive, secret, serialize,
+        stack, sys, update_local, update_remote, uuid, volunteer, when;
 
  // Definitions
 
@@ -133,8 +134,9 @@
              // messages it receives to 'comm' along with the internal 'state'
              // of the avar that received the message. We "hide" this method
              // by making it non-enumerable so that avars can be serialized,
-             // and unfortunately this means we have to require ECMAScript 5
-             // metaprogramming features in order to support remote execution.
+             // and unfortunately this means that old JavaScript engines that
+             // lack support for ECMAScript 5 metaprogramming cannot execute
+             // code remotely.
                 comm.call(this, state, obj);
                 return;
             }
@@ -288,6 +290,15 @@
                     if (inside.ready === true) {
                         x.comm({done: [], secret: secret});
                     }
+                } else if (args[0] instanceof AVar) {
+                    when(args[0], x).areready = function (evt) {
+                     // This function needs documentation.
+                        var f, x;
+                        f = this.val[0].val;
+                        x = this.val[1];
+                        f.call(x, evt);
+                        return;
+                    };
                 } else {
                     x.comm({
                         fail: 'Assigned value must be a function',
@@ -415,6 +426,159 @@
             }
             return (f !== undefined) ? f : val;
         });
+    };
+
+    dmap = function (f) {
+     // This function needs documentation.
+        return function (evt) {
+         // This function needs documentation.
+            var x, y;
+            x = (this.hasOwnProperty('isready')) ? this.val[0] : this;
+            y = avar({val: x.val});
+            y.onerror = function (message) {
+             // This function needs documentation.
+                return evt.fail(message);
+            };
+            y.onready = dply(function (key, val) {
+             // This function needs documentation.
+                y.val[key] = {f: f, x: val};
+                return;
+            });
+            y.onready = dply(function (key, val) {
+             // This function needs documentation.
+                val.y = val.f(val.x);
+                return;
+            });
+            y.onready = dply(function (key, val) {
+             // This function needs documentation.
+                x.val[key] = val.y;
+                return;
+            });
+            y.onready = function (y_evt) {
+             // This function needs documentation.
+                y_evt.exit();
+                return evt.exit();
+            };
+            return;
+        };
+    };
+
+    dply = function (f) {
+     // This function needs documentation.
+        return function (evt) {
+         // This function needs documentation.
+            var elements, g, key, n, x;
+            g = function (f, key, val) {
+             // This function needs documentation.
+                var temp = avar({val: {f: f, key: key, val: val}});
+                temp.onerror = function (message) {
+                 // This function needs documentation.
+                    return evt.fail(message);
+                };
+                temp.onready = function (evt) {
+                 // This function needs documentation.
+                    this.val.f(this.val.key, this.val.val);
+                    return evt.exit();
+                };
+                return temp;
+            };
+            elements = [];
+            x = (this.hasOwnProperty('isready')) ? this.val[0].val : this.val;
+            if (isArrayLike(x)) {
+                n = x.length;
+                for (key = 0; key < n; key += 1) {
+                    elements.push(g(f, key, x[key]));
+                }
+            } else if (x instanceof Object) {
+                for (key in x) {
+                    if (x.hasOwnProperty(key)) {
+                        elements.push(g(f, key, x[key]));
+                    }
+                }
+            } else {
+                return evt.fail('Cannot "ply" this value (' + x + ')');
+            }
+            when.apply(this, elements).onready = function (when_evt) {
+             // This function needs documentation.
+                when_evt.exit();
+                return evt.exit();
+            };
+            return;
+        };
+    };
+
+    dreduce = function (f) {
+     // This function needs documentation.
+        return function (evt) {
+         // This function needs documentation.
+            var x, y;
+            x = (this.hasOwnProperty('isready')) ? this.val[0] : this;
+            y = avar({val: x.val});
+            y.onerror = function (message) {
+             // This function needs documentation.
+                return evt.fail(message);
+            };
+            y.onready = function (evt) {
+             // This function needs documentation.
+                var flag, key, n, pairs, x;
+                flag = true;
+                pairs = [];
+                x = y.val;
+                if (isArrayLike(x)) {
+                    n = x.length;
+                    if ((n % 2) === 1) {
+                        pairs.push(x[0]);
+                        for (key = 1; key < n; key += 2) {
+                            pairs.push([x[key], x[key + 1]]);
+                        }
+                    } else {
+                        for (key = 0; key < n; key += 2) {
+                            pairs.push([x[key], x[key + 1]]);
+                        }
+                    }
+                } else if (x instanceof Object) {
+                    for (key in x) {
+                        if (x.hasOwnProperty(key)) {
+                            if (flag) {
+                                pairs.push([x[key]]);
+                            } else {
+                                (pairs[pairs.length - 1]).push(x[key]);
+                            }
+                            flag = (!flag);
+                        }
+                    }
+                } else {
+                    pairs.push([x]);
+                }
+                y.val = pairs;
+                return evt.exit();
+            };
+            y.onready = dmap(function (each) {
+             // This function needs documentation.
+                return (each instanceof Array) ? {f: f, x: each} : each;
+            });
+            y.onready = dmap(function (each) {
+             // This function needs documentation.
+                var flag;
+                flag = ((each !== null) &&
+                        (each !== undefined) &&
+                        (each.hasOwnProperty('f')) &&
+                        (each.hasOwnProperty('x')));
+                return (flag) ? each.f(each.x[0], each.x[1]) : each;
+            });
+            y.onready = function (y_evt) {
+             // This function needs documentation.
+                if (y.val.length > 1) {
+                    x.val = y.val;
+                    y_evt.exit();
+                    return evt.stay('Re-reducing ...');
+                }
+                x.val = y.val[0];
+                y_evt.exit();
+                return evt.exit();
+            };
+            return;
+        };
     };
 
     init = function (obj) {
@@ -1370,13 +1534,14 @@
      // This function constructs a temporary 'namespace' object and then
      // copies its methods and properties onto Method Q for "export".
 
-     // NOTE: New methods are coming soon: 'map', 'ply', and 'reduce' :-)
-
         var namespace;
 
         namespace = {
             avar:       avar,
             init:       init,
+            map:        dmap,
+            ply:        dply,
+            reduce:     dreduce,
             uuid:       uuid,
             volunteer:  volunteer,
             when:       when
