8f2e1dd9bf2b2c73e02df2fce02d7dc3311fbfc1
Sean Wilkinson <sean@mathbiol.org>
2012-08-10 17:22:21 -0500

    Re-activated the "volunteer" button

--- src/main.js
+++ src/main.js
@@ -2,19 +2,13 @@
 
 //- main.js ~~
 //
-//  Currently, this program's only purpose is to load "q.js", but future work
+//  Currently, this program's main purpose is to load "q.js", but future work
 //  will extend it to create a more immersive experience both for volunteers
-//  and for computational scientists. The recommended way to embed QMachine
-//  into your own webapps is to insert the following script tag into the body
-//  of your HTML page:
+//  and for computational scientists. The recommended way to embed QMachine's
+//  web service into your own web apps is to insert the following script tag
+//  into the body of your HTML page:
 //
 //      <script src="http://qmachine.org/q.js"></script>
-//
-//  NOTE: Inserting a script tag that points to "main.js" is "deprecated" --
-//  it works correctly for now, but load times are slower than loading "q.js"
-//  directly. Moreover, future versions of "main.js" will assume they are
-//  running as part of the presentation layer for QMachine's website, rather
-//  than as part of the infrastructure providing QMachine's web service.
 //
 //  NOTE: For maximum performance in your own app, embed "q-min.js", which is
 //  a minified, optimized version of "q.js". Typically, I use either Google's
@@ -24,19 +18,26 @@
 //  because I am not an active developer for either of the optimizers.
 //
 //                                                      ~~ (c) SRW, 23 May 2012
+//                                                  ~~ last updated 10 Aug 2012
 
-(function () {
+(function (global) {
     'use strict';
 
  // Pragmas
 
     /*jslint indent: 4, maxlen: 80, browser: true */
 
+    /*properties
+        Q, appendChild, body, call, checked, console, createElement, error,
+        exit, getElementById, global, hasOwnProperty, head, key, log, onclick,
+        onerror, onready, prototype, setAttribute, setTimeout, volunteer
+    */
+
  // Prerequisites
 
  // Declarations
 
-    var q;
+    var q, volunteer;
 
  // Definitions
 
@@ -44,6 +45,37 @@
 
     q.setAttribute('type', 'text/javascript');
     q.setAttribute('src', 'http://qmachine.org/q.js');
+
+    volunteer = function () {
+     // This function needs documentation.
+        if (Object.prototype.hasOwnProperty('Q') === false) {
+         // If Q hasn't loaded yet for some reason, try again later.
+            setTimeout(volunteer, 1000);
+            return;
+        }
+        if (document.getElementById('volunteer').checked === false) {
+            return;
+        }
+        var task = Object.prototype.Q.volunteer();
+        task.onerror = function (message) {
+         // This function needs documentation.
+            if ((global.hasOwnProperty('console')) &&
+                    (message !== 'Nothing to do ...')) {
+                global.console.error('Error:', message);
+            }
+            global.setTimeout(volunteer, 1000);
+            return;
+        };
+        task.onready = function (evt) {
+         // This function needs documentation.
+            if (global.hasOwnProperty('console')) {
+                global.console.log('Done:', this.key);
+            }
+            setTimeout(volunteer, 1000);
+            return evt.exit();
+        };
+        return task;
+    };
 
  // Invocations
 
@@ -53,6 +85,8 @@
         document.body.appendChild(q);
     }
 
+    document.getElementById('volunteer').onclick = volunteer;
+
  // Clean up after ourselves.
 
     q = null;
@@ -61,6 +95,15 @@
 
     return;
 
-}());
+}(Function.prototype.call.call(function (that) {
+    'use strict';
+ // See the bottom of "quanah.js" for documentation.
+    /*jslint indent: 4, maxlen: 80 */
+    /*global global: true */
+    if (this === null) {
+        return (typeof global === 'object') ? global : that;
+    }
+    return (typeof this.global === 'object') ? this.global : this;
+}, null, this)));
 
 //- vim:set syntax=javascript:
