6e90d3b87699e10fb9664d4376d77ceb44074b1e
Sean Wilkinson <sean@mathbiol.org>
2013-01-16 16:48:37 -0600

    Added CoffeeScript `submit` method tests

--- tests/tests.js
+++ tests/tests.js
@@ -6,8 +6,10 @@
 //  out over and over in the Chrome console. It's close enough to using Node.js
 //  that I lied to JSLint about it, hehe.
 //
+//  NOTE: I need to rewrite this junk so it uses Quanah ...
+//
 //                                                      ~~ (c) SRW, 28 Nov 2012
-//                                                  ~~ last updated 14 Jan 2013
+//                                                  ~~ last updated 16 Jan 2013
 
 (function () {
     'use strict';
@@ -18,11 +20,23 @@
 
  // Declarations
 
-    var mothership, run_test;
+    var exit, mothership, n, run_test;
 
  // Definitions
 
+    exit = function (code) {
+     // This function needs documentation.
+        /*global phantom: false */
+        n -= 1;
+        if (n === 0) {
+            setTimeout(phantom.exit, 0, code);
+        }
+        return;
+    };
+
     mothership = 'http://localhost:8177';
+
+    n = 6;
 
     run_test = function (y, f) {
      // This function needs documentation.
@@ -32,17 +46,17 @@
             console.log(message);
             if (message === y) {
                 homepage.close();
-                setTimeout(phantom.exit, 0);
+                exit(0);
             }
             if (message.slice(6) === 'Error:') {
-                setTimeout(phantom.exit, 0, 1);
+                exit(1);
             }
             return;
         };
         homepage.onError = function (message) {
          // This function needs documentation.
             console.error('Error:', message);
-            setTimeout(phantom.exit, 0, 1);
+            exit(1);
             return;
         };
         homepage.onResourceReceived = function (response) {
@@ -51,14 +65,14 @@
         };
         homepage.onResourceRequested = function (request) {
          // This function needs documentation.
-            console.log(request.method, request.url);
+            //console.log(request.method, request.url);
             return;
         };
         homepage.open(mothership, function (status) {
          // This function needs documentation.
             if (status !== 'success') {
                 console.error('Something went wrong:', status);
-                return phantom.exit(1);
+                return exit(1);
             }
             console.log('Running test ...');
             homepage.evaluate(f);
@@ -71,13 +85,113 @@
 
     console.log('NOTE: Remember to launch a worker for "sean"!');
 
-    run_test('Results: 4', function () {
+    run_test('Results: 1', function () {
      // This function needs documentation.
-        var x = window.QM.avar({box: 'sean', val: 2});
+        /*jslint browser: true */
+        var x = window.QM.avar({box: 'sean', val: 0});
+        x.Q(function (evt) {
+         // This function needs documentation.
+            this.val += 1;
+            return evt.exit();
+        }).Q(function (evt) {
+         // This function needs documentation.
+            console.log('Results:', this.val);
+            return evt.exit();
+        }).on('error', function (message) {
+         // This function needs documentation.
+            console.error('Error:', message);
+            return;
+        });
+        return;
+    });
+
+    run_test('Results: 2', function () {
+     // This function needs documentation.
+        /*jslint browser: true */
+        var x = window.QM.avar();
+        x.box = 'sean';
+        x.val = 0;
         x.Q(function (evt) {
          // This function needs documentation.
             this.val += 2;
             return evt.exit();
+        }).Q(function (evt) {
+         // This function needs documentation.
+            console.log('Results:', this.val);
+            return evt.exit();
+        }).on('error', function (message) {
+         // This function needs documentation.
+            console.error('Error:', message);
+            return;
+        });
+        return;
+    });
+
+    run_test('Results: 3', function () {
+     // This function needs documentation.
+        /*jslint browser: true */
+        window.QM.submit({
+            box: 'sean',
+            f: function (x) {
+             // This function needs documentation.
+                return x + 2;
+            },
+            x: 1
+        }).Q(function (evt) {
+         // This function needs documentation.
+            console.log('Results:', this.val);
+            return evt.exit();
+        }).on('error', function (message) {
+         // This function needs documentation.
+            console.error('Error:', message);
+            return;
+        });
+        return;
+    });
+
+    run_test('Results: 4', function () {
+     // This function needs documentation.
+        /*jslint browser: true */
+        window.QM.submit(2, function (x) {
+         // This function needs documentation.
+            return x + 2;
+        }, 'sean').Q(function (evt) {
+         // This function needs documentation.
+            console.log('Results:', this.val);
+            return evt.exit();
+        }).on('error', function (message) {
+         // This function needs documentation.
+            console.error('Error:', message);
+            return;
+        });
+        return;
+    });
+
+    run_test('Results: 5', function () {
+     // This function needs documentation.
+        /*jslint browser: true */
+        window.QM.submit({
+            box: 'sean',
+            f: '(x) -> x + 2',
+            x: 3
+        }).Q(function (evt) {
+         // This function needs documentation.
+            console.log('Results:', this.val);
+            return evt.exit();
+        }).on('error', function (message) {
+         // This function needs documentation.
+            console.error('Error:', message);
+            return;
+        });
+        return;
+    });
+
+    run_test('Results: 6', function () {
+     // This function needs documentation.
+        window.QM.submit({
+            box: 'sean',
+            f: '(x) -> x + 2',
+            x: 4
         }).Q(function (evt) {
          // This function needs documentation.
             console.log('Results:', this.val);
