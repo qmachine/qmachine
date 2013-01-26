3f71a4a21cfc875c13426580a96fde0872b60b68
Sean Wilkinson <sean@mathbiol.org>
2013-01-26 17:32:44 -0600

    Updated TCGA manifest file to point to the "v1.qmachine.org" deployment

--- etc/tcga-web-toolbox/manifest.js
+++ etc/tcga-web-toolbox/manifest.js
@@ -2,7 +2,7 @@
 
 //- manifest.js ~~
 //                                                      ~~ (c) SRW, 11 Jul 2012
-//                                                  ~~ last updated 19 Sep 2012
+//                                                  ~~ last updated 26 Jan 2013
 
 /*jslint indent: 4, maxlen: 80 */
 /*global manifest: false */
@@ -12,7 +12,7 @@ manifest([
         "author": "Sean Wilkinson",
         "name": "QMachine",
         "description": "The World's Most Relaxed Supercomputer",
-        "url": "https://qmachine.org/q.js"
+        "url": "https://v1.qmachine.org/q.js"
     }
 ]);
 
