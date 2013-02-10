daf2a15b0715ecec00c3a86ba6e9933620399134
Sean Wilkinson <sean@mathbiol.org>
2013-02-10 04:44:50 -0600

    Cleaned up the "web-service" target a little bit

--- src/web-service/server.js
+++ src/web-service/server.js
@@ -2,7 +2,7 @@
 
 //- server.js ~~
 //                                                      ~~ (c) SRW, 06 Oct 2012
-//                                                  ~~ last updated 21 Dec 2012
+//                                                  ~~ last updated 10 Feb 2013
 
 (function () {
     'use strict';
@@ -26,7 +26,7 @@
  // Invocations
 
     if (process.env.IP !== undefined) {
-     // This is for debugging on Cloud9, if memory serves ...
+     // This is for debugging on Cloud9.
         options.hostname = process.env.IP;
     }
 
@@ -36,20 +36,20 @@
     }
 
     if (process.env.QM_API_STRING !== undefined) {
-     // This is a custom environment variable I use with Heroku and AppFog.
+     // This is a custom environment variable I define prior to deployment.
         options.enable_api_server = true;
         options.enable_CORS = true;
         options.persistent_storage = JSON.parse(process.env.QM_API_STRING);
     }
 
     if (process.env.QM_WWW_STRING !== undefined) {
-     // This is a custom environment variable I use with Heroku and AppFog.
+     // This is a custom environment variable I define prior to deployment.
         options.enable_www_server = true;
         options.static_content = process.env.QM_WWW_STRING;
     }
 
     if (process.env.VMC_APP_PORT !== undefined) {
-     // This is for use with AppFog.
+     // This is for use with Cloud Foundry platforms.
         options.hostname = null;
         options.port = process.env.VMC_APP_PORT;
     }
