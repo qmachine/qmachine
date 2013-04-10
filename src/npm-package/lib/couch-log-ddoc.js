67902a74883acc7d303c1277f0602d4d9326969d
Sean Wilkinson <sean@mathbiol.org>
2013-04-10 14:53:27 -0500

    Created "box_frequency" and "ip_frequency" CouchDB views

--- src/npm-package/lib/couch-log-ddoc.js
+++ src/npm-package/lib/couch-log-ddoc.js
@@ -2,22 +2,22 @@
 
 //- couch-log-ddoc.js ~~
 //                                                      ~~ (c) SRW, 31 Mar 2013
-//                                                  ~~ last updated 09 Apr 2013
+//                                                  ~~ last updated 10 Apr 2013
 
 (function () {
     'use strict';
 
  // Pragmas
 
-    /*global exports: false */
+    /*global exports: false, sum: false */
 
     /*jshint maxparams: 2, quotmark: single, strict: true */
 
     /*jslint couch: true, indent: 4, maxlen: 80, nomen: true */
 
     /*properties
-        hasOwnProperty, _id, ip, ips, length, lists, map, reduce, shows, split,
-        updates, url, views
+        box_frequency, hasOwnProperty, _id, ip, ip_frequency, ips, lists, map,
+        reduce, replace, shows, split, updates, url, views
     */
 
  // Out-of-scope definitions
@@ -31,22 +31,42 @@
     exports.updates = {};
 
     exports.views = {
-        ips: {
+
+        box_frequency: {
             map: function (doc) {
              // This function needs documentation.
-                var box, flag, key, status;
-                flag = ((doc.hasOwnProperty('ip'))  &&
-                        (doc.hasOwnProperty('url')));
-                if (flag === true) {
-                    emit(doc.ip.split(', ')[0], doc.url);
+                var pattern;
+                if (doc.hasOwnProperty('url')) {
+                    pattern = /^\/box\/([\w\-]+)\?/;
+                    doc.url.replace(pattern, function (match, box) {
+                     // This function needs documentation.
+                        emit(box, 1);
+                        return;
+                    });
+                    return;
                 }
                 return;
             },
             reduce: function (keys, values) {
              // This function needs documentation.
-                return values.length;
+                return sum(values);
+            }
+        },
+
+        ip_frequency: {
+            map: function (doc) {
+             // This function needs documentation.
+                if (doc.hasOwnProperty('ip')) {
+                    emit(doc.ip.split(', ')[0], 1);
+                }
+                return;
+            },
+            reduce: function (keys, values) {
+             // This function needs documentation.
+                return sum(values);
             }
         }
+
     };
 
  // That's all, folks!
