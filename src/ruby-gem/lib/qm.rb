22bf57d863cd1859edf5759035f3867aaae1de19
Sean Wilkinson <sean@mathbiol.org>
2013-04-15 21:57:31 -0500

    Added placeholders and an actual module definition to Ruby gem

--- src/ruby-gem/lib/qm.rb
+++ src/ruby-gem/lib/qm.rb
@@ -2,11 +2,28 @@
 
 #-  qm.rb ~~
 #                                                       ~~ (c) SRW, 12 Apr 2013
-#                                                   ~~ last updated 12 Apr 2013
+#                                                   ~~ last updated 15 Apr 2013
 
-def placeholder()
-  # This function needs documentation.
-    puts '(placeholder)'
+module QM
+
+    def self::launch_client()
+      # This function needs documentation.
+        puts '(placeholder: `launch_client`)';
+        return;
+    end
+
+    def self::launch_service(*obj)
+      # This function needs documentation.
+        puts '(placeholder: `launch_service`)';
+        return;
+    end
+
+    def self::roll_up(public_html, json_file)
+      # This function needs documentation.
+        puts '(placeholder: `roll_up`)';
+        return;
+    end
+
 end
 
 #-  vim:set syntax=ruby:
