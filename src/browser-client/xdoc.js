dad5d80714f675544d0e331b7f2f07a421f01849
Sean Wilkinson <sean@mathbiol.org>
2013-04-24 10:34:49 -0500

    Created a placeholder script

---
+++ src/browser-client/xdoc.js
@@ -0,0 +1,42 @@
+//- JavaScript source code
+
+//- xdoc.js ~~
+//
+//  This version of QMachine will embed the communications layer in an iframe
+//  using Cross-Document Messaging.
+//
+//                                                      ~~ (c) SRW, 24 Apr 2013
+//                                                  ~~ last updated 24 Apr 2013
+
+(function () {
+    'use strict';
+
+ // Pragmas
+
+ // Prerequisites
+
+ // Declarations
+
+ // Definitions
+
+ // Prototype definitions
+
+ // Out-of-scope definitions
+
+ // Invocations
+
+    (function () {
+
+     // (placeholder)
+
+        return;
+
+    }());
+
+ // That's all, folks!
+
+    return;
+
+}());
+
+//- vim:set syntax=javascript:
