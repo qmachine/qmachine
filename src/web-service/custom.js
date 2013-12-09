a3ef5237493f52f76ea82fa88321a08f577f9233
Sean Wilkinson <sean@mathbiol.org>
2013-12-09 16:07:37 -0600

    Record "Content-Length" header in web-service demo

--- src/web-service/custom.js
+++ src/web-service/custom.js
@@ -13,9 +13,9 @@
 
     /*properties
         cf_connecting_ip, 'cf-connecting-ip', cf_ipcountry, 'cf-ipcountry',
-        cf_ray, 'cf-ray', cf_visitor, 'cf-visitor', connection, env,
-        hasOwnProperty, headers, host, ip, log, method, parse, remoteAddress,
-        replace, split, timestamp, url, 'x-forwarded-for'
+        cf_ray, 'cf-ray', cf_visitor, 'cf-visitor', connection, content_length,
+        'content-length', env, hasOwnProperty, headers, host, ip, log, method,
+        parse, remoteAddress, replace, split, timestamp, url, 'x-forwarded-for'
     */
 
  // Module definitions
@@ -25,7 +25,7 @@
      // if logging is enabled. This function is optional, however, because
      // QMachine provides a default logging function; feel free to delete or
      // adapt it as needed :-)
-        var data = {
+        var y = {
             host: request.headers.host,
             method: request.method,
             timestamp: new Date(),
@@ -35,31 +35,34 @@
          // This header is specific to CloudFlare (www.cloudflare.com), but
          // storing the header with a hyphen inside can cause problems in query
          // languages like Hive. Thus, we use a hyphen convention.
-            data.cf_connecting_ip = request.headers['cf-connecting-ip'];
+            y.cf_connecting_ip = request.headers['cf-connecting-ip'];
         }
         if (request.headers.hasOwnProperty('cf-ipcountry')) {
          // This header is specific to CloudFlare (www.cloudflare.com), too.
-            data.cf_ipcountry = request.headers['cf-ipcountry'];
+            y.cf_ipcountry = request.headers['cf-ipcountry'];
         }
      /*
         if (request.headers.hasOwnProperty('cf-ray')) {
          // This header is specific to CloudFlare (www.cloudflare.com), too.
          // It has been commented out here because it's mainly intended for
          // debugging with CF Support. Read more at http://goo.gl/wMfrD8.
-            data.cf_ray = request.headers['cf-ray'];
+            y.cf_ray = request.headers['cf-ray'];
         }
         if (request.headers.hasOwnProperty('cf-visitor')) {
          // This header is specific to CloudFlare (www.cloudflare.com), too.
          // Read more at http://goo.gl/N7W6wJ.
-            data.cf_visitor = JSON.parse(request.headers['cf-visitor']);
+            y.cf_visitor = JSON.parse(request.headers['cf-visitor']);
         }
      */
-        if (request.headers.hasOwnProperty('x-forwarded-for')) {
-            data.ip = request.headers['x-forwarded-for'].split(',')[0];
-        } else {
-            data.ip = request.connection.remoteAddress;
+        if (request.headers.hasOwnProperty('content-length')) {
+            y.content_length = parseInt(request.headers['content-length'], 10);
         }
-        return data;
+        if (request.headers.hasOwnProperty('x-forwarded-for')) {
+            y.ip = request.headers['x-forwarded-for'].split(',')[0];
+        } else {
+            y.ip = request.connection.remoteAddress;
+        }
+        return y;
     };
 
     exports.parse = function (x) {
