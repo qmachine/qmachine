c2b18d8ce5d828df7dafd111958ea80b270d61a2
Sean Wilkinson <sean@mathbiol.org>
2015-01-11 02:11:02 -0600

    More API tests

--- tests/api-tests.js
+++ tests/api-tests.js
@@ -119,7 +119,7 @@
  // Tests
 
     test([{
-        'label': 'Simple test for `get_avar`',
+        'label': 'Simple test for the `get_avar` route',
         'req': {
             'method': 'GET',
             'path': '/box/simpletest?key=get_avar'
@@ -131,7 +131,7 @@
     }]);
 
     test([{
-        'label': 'Simple test for `get_jobs`',
+        'label': 'Simple test for the `get_jobs` route',
         'req': {
             'method': 'GET',
             'path': '/box/simpletest?status=get_jobs'
@@ -143,7 +143,7 @@
     }]);
 
     test([{
-        'label': 'Simple test for `set_avar`',
+        'label': 'Simple test for the `set_avar` route',
         'req': {
             'data': '{"box":"simpletest","key":"set_avar","val":"ooga"}',
             'headers': {
@@ -183,6 +183,203 @@
         'res': {
             'data': '{"box":"simpletest","key":"regular_avar","val":2}',
             'statusCode': 200
+        }
+    }]);
+
+    test([{
+        'label': 'GET requests must specify a "key" or a "status"',
+        'req': {
+            'method': 'GET',
+            'path': '/box/justabox?'
+        },
+        'res': {
+            'data': '',
+            'statusCode': 444
+        }
+    }]);
+
+    test([{
+        'label': 'GET requests cannot specify both a "key" and a "status"',
+        'req': {
+            'method': 'GET',
+            'path': '/box/toomanyparameters?key=hello&status=world'
+        },
+        'res': {
+            'data': '',
+            'statusCode': 444
+        }
+    }]);
+
+    test([{
+        'label': 'GET requests cannot specify both a "status" and a "key"',
+        'req': {
+            'method': 'GET',
+            'path': '/box/sretemarapynamoot?status=hello&key=world'
+        },
+        'res': {
+            'data': '',
+            'statusCode': 444
+        }
+    }]);
+
+    test([{
+        'label': 'POST requests must specify a "key" in URL',
+        'req': {
+            'data': '{"box":"justabox","key":"abc123","val":2}',
+            'headers': {
+                'Content-Length': 41,
+                'Content-Type': 'application/json'
+            },
+            'method': 'POST',
+            'path': '/box/justabox?'
+        },
+        'res': {
+            'data': '',
+            'statusCode': 444
+        }
+    }]);
+
+    test([{
+        'label': 'In `set_avar` route, "key" in body must match URL param',
+        'req': {
+            'data': '{"box":"justabox","key":"csharp","val":2}',
+            'headers': {
+                'Content-Length': 41,
+                'Content-Type': 'application/json'
+            },
+            'method': 'POST',
+            'path': '/box/mismatchedkeys?key=abc123'
+        },
+        'res': {
+            'data': '',
+            'statusCode': 444
+        }
+    }]);
+
+    test([{
+        'label': 'In `get_avar` route, "box" supports the expected characters',
+        'req': {
+            'method': 'GET',
+            'path': '/box/ABCDEFGHIJKLMNOPQRSTUVWXYZ' +
+                    'abcdefghijklmnopqrstuvwxyz0123456789_-?key=deadbeef'
+        },
+        'res': {
+            'data': '{}',
+            'statusCode': 200
+        }
+    }]);
+
+    test([{
+        'label': 'In `get_jobs` route, "box" supports the expected characters',
+        'req': {
+            'method': 'GET',
+            'path': '/box/ABCDEFGHIJKLMNOPQRSTUVWXYZ' +
+                    'abcdefghijklmnopqrstuvwxyz0123456789_-?status=waiting'
+        },
+        'res': {
+            'data': '[]',
+            'statusCode': 200
+        }
+    }]);
+
+    test([{
+        'label': 'In `set_avar` route, "box" supports the expected characters',
+        'req': {
+            'data': '{"box":"ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrst' +
+                    'uvwxyz0123456789_-","key":"abc123","val":2}',
+            'headers': {
+                'Content-Length': 97,
+                'Content-Type': 'application/json'
+            },
+            'method': 'POST',
+            'path': '/box/ABCDEFGHIJKLMNOPQRSTUVWXYZ' +
+                    'abcdefghijklmnopqrstuvwxyz0123456789_-?key=abc123'
+        },
+        'res': {
+            'data': '',
+            'statusCode': 201
+        }
+    }]);
+
+    test([{
+        'label': 'In `get_avar` route, "box" cannot contain a `.` character',
+        'req': {
+            'method': 'GET',
+            'path': '/box/mongo.badness?key=deadbeef'
+        },
+        'res': {
+            'data': '',
+            'statusCode': 444
+        }
+    }]);
+
+    test([{
+        'label': 'In `get_jobs` route, "box" cannot contain a `.` character',
+        'req': {
+            'method': 'GET',
+            'path': '/box/mongo.badness?status=waiting'
+        },
+        'res': {
+            'data': '',
+            'statusCode': 444
+        }
+    }]);
+
+    test([{
+        'label': 'In `set_avar` route, "box" cannot contain a `.` character',
+        'req': {
+            'data': '{"box":"mongo.badness","key":"abc123","val":2}',
+            'headers': {
+                'Content-Length': 46,
+                'Content-Type': 'application/json'
+            },
+            'method': 'POST',
+            'path': '/box/mongo.badness?key=abc123'
+        },
+        'res': {
+            'data': '',
+            'statusCode': 444
+        }
+    }]);
+
+    test([{
+        'label': 'In `get_avar` route, "box" cannot contain a `&` character',
+        'req': {
+            'method': 'GET',
+            'path': '/box/param&badness?key=deadbeef'
+        },
+        'res': {
+            'data': '',
+            'statusCode': 444
+        }
+    }]);
+
+    test([{
+        'label': 'In `get_jobs` route, "box" cannot contain a `&` character',
+        'req': {
+            'method': 'GET',
+            'path': '/box/param&badness?status=waiting'
+        },
+        'res': {
+            'data': '',
+            'statusCode': 444
+        }
+    }]);
+
+    test([{
+        'label': 'In `set_avar` route, "box" cannot contain a `&` character',
+        'req': {
+            'data': '{"box":"param&badness","key":"abc123","val":2}',
+            'headers': {
+                'Content-Length': 46,
+                'Content-Type': 'application/json'
+            },
+            'method': 'POST',
+            'path': '/box/param&badness?key=abc123'
+        },
+        'res': {
+            'data': '',
+            'statusCode': 444
         }
     }]);
 
