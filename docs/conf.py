914b0fd8799c93f326454db80a40260933a42222
Sean Wilkinson <sean@mathbiol.org>
2015-02-16 15:17:59 -0600

    Use single quotes, not double quotes

--- docs/conf.py
+++ docs/conf.py
@@ -42,7 +42,7 @@ master_doc = 'index'
 # General information about the project.
 
 project = u'QMachine'
-copyright = u'2014-' + time.strftime("%Y") + ', Sean Wilkinson'
+copyright = u'2014-' + time.strftime('%Y') + ', Sean Wilkinson'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
